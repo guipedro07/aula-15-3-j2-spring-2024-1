e32e7b88777674c26b671c03886bec1e06a26d2f
Guilherme Pedro <132401438+guipedro07@users.noreply.github.com>
2024-03-15 19:46:28 +0000

    15/03 aula

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        SpringApplication.run(Main.class, args);
     }
 }
-
